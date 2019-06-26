9a062b39e6e3bf9216c78b362634ac56faf89629
J Lobos Vega <jalv.vg@gmail.com>
2019-06-26 15:37:45 -0400

    ondel's files

---
+++ projectX2-Ondel_Files/client/build/precache-manifest.972734dbd3cdd545fe4179c01f033648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0053fed3bc0fc460ee8d90d9400e4295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e6dec44dd6414548f02",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "9e6dec44dd6414548f02",
+    "url": "/static/js/main.fe6ddb86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
